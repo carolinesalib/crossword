a6a04799ce2bbcea4fd483b14b2a2c7d06c15f36
Caroline <carolinesalibc@gmail.com>
2025-08-11 19:51:17 -0600

    Setup e2e tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  # Run in visible mode if VISIBLE_TESTS environment variable is set
+  # Otherwise run in headless mode (good for CI)
+  if ENV["VISIBLE_TESTS"]
+    driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+  else
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
+  end
 end
